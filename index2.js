91c64fd4d811711090bd35fcf12423de96612a41
Yvesmorello <124888523+Yvesmorello@users.noreply.github.com>
2024-05-10 01:23:30 +0000

    atividade-Todo-2bim

--- index2.js
+++ index2.js
@@ -1,36 +1,35 @@
 function formatarData(data) {
-    const options = {
-      day: 'numeric',
-      month: 'numeric',
-      year: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric',
-    };
-    return new Intl.DateTimeFormat('pt-BR', options).format(data);
+  const options = {
+    day: 'numeric',
+    month: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+  };
+  return new Intl.DateTimeFormat('pt-BR', options).format(data);
 }
 
 function atualizarContadorDataExibir() {
-    let visitorsCounter = 0;
-    let lastVisit = "Nunca acessado";
-  
-    const savedCount = localStorage.getItem('visitorsCounter');
-    
-    if (savedCount) {
-      const dados = JSON.parse(savedCount);
-      visitorsCounter = dados.count;
-      lastVisit = dados.lastVisit;
-    }
-  
-    visitorsCounter++;
-    lastVisit = formatarData(new Date());
-  
-    const dados = {
-      count: visitorsCounter,
-      lastVisit: lastVisit,
-    };
-  
-    localStorage.setItem('visitorsCounter', JSON.stringify(dados));
+  let visitorsCounter = 0;
+  let lastVisit = "Nunca acessado";
 
-    document.getElementById('footerCounter').innerHTML = "Esta página foi visitada " + visitorsCounter + " vezes. A última visita foi: " + lastVisit;
+  const savedCount = localStorage.getItem('visitorsCounter');
+  
+  if (savedCount) {
+    const dados = JSON.parse(savedCount);
+    visitorsCounter = dados.count;
+    lastVisit = dados.lastVisit;
+  }
+
+  visitorsCounter++;
+  lastVisit = formatarData(new Date());
+
+  const dados = {
+    count: visitorsCounter,
+    lastVisit: lastVisit,
+  };
+
+  localStorage.setItem('visitorsCounter', JSON.stringify(dados));
+
+  document.getElementById('footerCounter').innerHTML = "Esta página foi visitada " + visitorsCounter + " vezes. A última visita foi: " + lastVisit;
 }
-
